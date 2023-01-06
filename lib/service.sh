4e8470737a2ec13d669939740bb319c5665f855a
Alexander Bragin <alefbragin@gmail.com>
2023-01-06 18:16:20 +0700

    wip: rn to varname

--- lib/service.sh
+++ lib/service.sh
@@ -10,13 +10,13 @@ shef__function_aliases \
 	service_set_reload \
 	service_ensure
 
-shef__service_variable_name() {
+shef__service_varname() {
 	shef__stdin_arg "service_$1" tr - _ \
 		|| shef__die "make service variable name from: '$1'"
 }
 
 shef__service_set_reload() {
-	shef__service_set_reload__name="$(shef__service_variable_name "$1")" || shef__die
+	shef__service_set_reload__name="$(shef__service_varname "$1")" || shef__die
 	shef__state_set "${shef__service_set_reload__name}_needs_reload" "$2"
 }
 
@@ -29,7 +29,7 @@ shef__service_ensure() {
 	else
 		shef__log_already "service already active: '$1'"
 
-		shef__service_ensure__name="$(shef__service_variable_name "$1")" || shef__die
+		shef__service_ensure__name="$(shef__service_varname "$1")" || shef__die
 
 		if shef__check_state "${shef__service_ensure__name}_needs_reload"; then
 			systemctl reload "$1" \
