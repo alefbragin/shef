e7b8ac8f63636d3f6aa36862ecc080c09c453f00
Alexander Bragin <alefbragin@gmail.com>
2023-08-05 00:47:06 +0800

    feat(library): add ability to ensure_service by privileged user

--- lib/shef/systemd/_impl/ensure_service.sh
+++ lib/shef/systemd/_impl/ensure_service.sh
@@ -9,15 +9,21 @@
 #.	./make_service_state_name.sh
 
 shef__ensure_service() {
+	if [ "$1" = '--privileged' ]; then
+		shef__ensure_service__doas_privileged=sudo
+		shift
+	fi
 	if ! systemctl is-active "$1" > /dev/null; then
-		systemctl start "$1" || die "start service: '$1'"
+		$shef__ensure_service__doas_privileged \
+			systemctl start "$1" || die "start service: '$1'"
 		shef__log_changed "service was successfully started: '$1'"
 	else
 		shef__log_already "service is already active: '$1'"
 
 		shef_impl__make_service_state_name "$1" restart
 		if shef__state_check "$shef_impl__make_service_state_name__result"; then
-			systemctl restart "$1" || die "restart service: '$1'"
+			$shef__ensure_service__doas_privileged \
+				systemctl restart "$1" || die "restart service: '$1'"
 			shef__state_set "$shef_impl__make_service_state_name__result" 0
 			shef__log_changed "service was successfully restarted: '$1'"
 		else
@@ -25,7 +31,8 @@ shef__ensure_service() {
 
 			shef_impl__make_service_state_name "$1" reload
 			if shef__state_check "$shef_impl__make_service_state_name__result"; then
-				systemctl reload "$1" || die "reload service: '$1'"
+				$shef__ensure_service__doas_privileged \
+					systemctl reload "$1" || die "reload service: '$1'"
 				shef__state_set "$shef_impl__make_service_state_name__result" 0
 				shef__log_changed "service was successfully reloaded: '$1'"
 			else
